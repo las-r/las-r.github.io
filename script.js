edb1dc62eadea6a75df275732a499e502465430c
Nayif Ehan <128936184+las-r@users.noreply.github.com>
2026-06-10 00:08:57 -0400

    Fix repo count limit

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 async function loadRepos() {
     const username = "las-r";
-    const res = await fetch(`https://api.github.com/users/${username}/repos`);
-    const repos = await res.json();
-    const filtered = repos.filter(repo => !repo.fork && !repo.archived);
+    let allRepos = [];
+    
+    let page = 1;
+    let keepFetching = true;
+    while (keepFetching) {
+        const res = await fetch(`https://api.github.com/users/${username}/repos?per_page=100&page=${page}`);
+        const repos = await res.json();
+        
+        if (repos.length === 0) {
+            keepFetching = false;
+        } else {
+            allRepos = allRepos.concat(repos);
+            page++;
+        }
+    }
+
+    const filtered = allRepos.filter(repo => !repo.fork && !repo.archived);
     filtered.sort((a, b) => new Date(b.pushed_at) - new Date(a.pushed_at));
+    
     const container = document.getElementById("projects");
     const maxRepos = 10;
 
-    filtered.slice(0, maxRepos).forEach(filtered => {
+    filtered.slice(0, maxRepos).forEach(repo => {
         const div = document.createElement("div");
 
         div.innerHTML = `
-            <h3><a href="${filtered.html_url}" target="_blank">${filtered.name}</a></h3>
-            <p>${filtered.description || "No description provided."}</p>
+            <h3><a href="${repo.html_url}" target="_blank">${repo.name}</a></h3>
+            <p>${repo.description || "No description provided."}</p>
             <p class="links">
-                <a href="${filtered.html_url}" target="_blank">Source</a>
-                ${filtered.homepage ? ` | <a href="${filtered.homepage}" target="_blank">Webpage</a>` : ""}
+                <a href="${repo.html_url}" target="_blank">Source</a>
+                ${repo.homepage ? ` | <a href="${repo.homepage}" target="_blank">Webpage</a>` : ""}
             </p>
         `;
 
